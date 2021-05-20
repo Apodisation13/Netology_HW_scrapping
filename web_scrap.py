24bb373fec5dcd13c7da711f977ae8c6274b0bbe
Pavel Soloshenko <apodisation13@gmail.com>
2021-05-20 17:17:04 +0300

    added article_page_parcer

--- web_scrap.py
+++ web_scrap.py
@@ -2,14 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 
 
-desired_tags = {'мы', 'javascript', 'java', 'python', 'компьютер'}
+desired_tags = {'javascript', 'python', 'сервер'}  # слово сервер - часто встречается в самой статье))
 
 response = requests.get('https://habr.com/ru/all/')
 if not response.ok:
     raise ValueError('response is not valid')
 soup = BeautifulSoup(response.text, 'html.parser')
-
-# href_list = []
 
 for article in soup.find_all('article'):
 
@@ -19,34 +17,49 @@ for article in soup.find_all('article'):
     title = article.find('h2', class_='post__title')
 
     head = title.find(class_='post__title_link').text   # заголовок статьи
-    # print(head)
 
-    if hubs & desired_tags:
+    href = title.find('a').attrs.get('href')  # ссылка на статью
+
+    hubs_match = True  # флаг для ускорения поиска - нашлось совпадение по хабам
+    head_preview_match = False  # совпадение по head  или статья
+    page_body_match = False  # совпадение только в самой статье после перехода на неё
+
+    if not hubs & desired_tags:
+        hubs_match = False
+        wall = article.find('div', class_='post__body post__body_crop').text  # текст превью статьи
+
+        for hub in desired_tags:
+            if hub in wall.lower() or hub in head.lower():
+                head_preview_match = True
+                matched_head_preview_hub = hub
+                break
+
+        if not head_preview_match:
+
+            article_page = requests.get(href)
+            if not article_page.ok:
+                raise ValueError('response is not valid')
+            soup_page = BeautifulSoup(article_page.text, 'html.parser')
+
+            article_data = soup_page.find('article')
+            content = article_data.find('div', class_='post__body post__body_full').text.lower()
+
+            for hub in desired_tags:
+                if hub in content:
+                    page_body_match = True
+                    matched_body_hub = hub
+                    break
+
+    if head_preview_match:
+        print(f"Совпадение в заголовке или в превью - {{'{matched_head_preview_hub}'}}")
+    elif hubs_match:
         print(f'Совпадение по тегу - {hubs & desired_tags}')
-        href = title.find('a').attrs.get('href')  # ссылка на статью
-
-        header = article.find('header', class_='post__meta')
-        time = header.find('span', class_='post__time').text  # время статьи
-
-        print(f'<{time}>, <{head}>, <{href}>')
+    elif page_body_match:
+        print(f"Совпадение в самой статье - {{'{matched_body_hub}'}}")
+    else:
         continue
 
-    wall = article.find('div', class_='post__body post__body_crop').text  # текст превью статьи
-    # print(wall)
+    time_head = article.find('header', class_='post__meta')
+    time = time_head.find('span', class_='post__time').text  # время статьи
 
-    status_match = False
-    for hub in desired_tags:
-        if hub in wall.lower() or hub in head.lower():
-            status_match = True
-            print(f"Совпадение в заголовке или в превью - {{'{hub}'}}")
-
-            href = title.find('a').attrs.get('href')  # ссылка на статью
-
-            header = article.find('header', class_='post__meta')
-            time = header.find('span', class_='post__time').text  # время статьи
-
-            print(f'<{time}>, <{head}>, <{href}>')
-            break
-
-    if status_match:
-        continue
+    print(f'<{time}>, <{head}>, <{href}>')
